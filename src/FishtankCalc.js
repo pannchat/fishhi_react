785ada5039e4bef11addcfe5694207b41b306f48
pannchat <pannchat@likelion.org>
2021-07-14 18:48:53 +0900

    fishtankCalc.js css

--- src/FishtankCalc.js
+++ src/FishtankCalc.js
@@ -9,15 +9,27 @@ const FishtankCalc = () => {
         width:70px;
         border: solid 1px #8c98a4;
         text-align: center;
+        // 위아래 버튼없애기
+        &::-webkit-outer-spin-button,
+            &::-webkit-inner-spin-button{
+                -webkit-appearance: none;
+                margin: 0;
+            }
+        &+div{
+                display:inline-block;
+                width:16px;
+                padding: 0 2px;
+                vertical-align: bottom;
+                font-size:0.7rem;
+        }
     `;
 
     return(
     <>
-        {/* <NavBar /> */}
+        <NavBar />
         <header className="header">
             dosage calculator
         </header>
-        <input placeholder="test"/>
         <section className="main-section">
             <div className="flex-box--main">
                 <div className="main-section__tank-preview">
@@ -32,16 +44,16 @@ const FishtankCalc = () => {
                         </div>
                     </div>
                     <div className="main-section__tank-size">
-                        <TankInput type="number" placeholder="가로"/>
-                        <TankInput type="number" placeholder="세로"/>
-                        <TankInput type="number" placeholder="높이"/>
+                        <TankInput type="number" placeholder="가로"/><div>cm</div>
+                        <TankInput type="number" placeholder="세로"/><div>cm</div>
+                        <TankInput type="number" placeholder="높이"/><div>cm</div>
                     </div>
                     <div className="main-section__detailed">
-                        <TankInput type="number" placeholder="유리두께"/>
-                        <TankInput type="number" placeholder="바닥재"/>
-                        <TankInput type="number" placeholder="만수위까지"/>
+                        <TankInput type="number" placeholder="유리두께"/><div>T</div>
+                        <TankInput type="number" placeholder="바닥재"/><div>cm</div>
+                        <TankInput type="number" placeholder="만수위까지"/><div>cm</div>
                     </div>
-                    <div className="flex-box">
+                    <div className="flex-box--func">
                         <span>상세 설정</span>
                         <label className="switch-button">상세설정 Toggle
                         <input type="checkbox" id="checkbox"/>
@@ -49,7 +61,7 @@ const FishtankCalc = () => {
                         </label>
                     </div>
                     <div className="flex-box--func">
-                        <input type="button" value="계산"/>
+                        <input id="calc-btn" type="button" value="계산"/>
                     </div>
                     <div className="main-secion__calc-capacity">
                         {capacity}
